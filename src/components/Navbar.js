3555eba025ef2596cab7507a29665e7625dcfd1e
fajfara <a.fajfar95@gmail.com>
2018-12-29 23:45:58 +0100

    testing some stuff

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,1 +1,22 @@
-// Comment for push + change for test branch
+import React from 'react';
+import logo from '../img/logo.svg';
+
+const Navbar = () => {
+    return (
+        <div className="logo-and-menu container text-center">
+            <div className="container">
+                <img src={logo} className="logo" alt="logo" />
+                <div className="main-menu">
+                    <ul className="nav-links">
+                        <li><a href="/">HOME</a></li>
+                        <li><a href="/">ABOUT US</a></li>
+                        <li><a href="/">MENU</a></li>
+                        <li><a href="/">CONTACT</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar;
