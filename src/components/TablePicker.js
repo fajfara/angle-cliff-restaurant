c55c73d10d43b3be8ffc5ac3e6e01e521746ead1
fajfara <a.fajfar95@gmail.com>
2019-01-03 22:21:29 +0100

    minor fixes

--- src/components/TablePicker.js
+++ src/components/TablePicker.js
@@ -1,25 +1,9 @@
 import React, { Component } from 'react'
-import anime from 'animejs';
 
 class TablePicker extends Component {
-    playAnimation() {
-        let morphing = anime({
-            targets: '#test',
-            d: [
-                { value: 'M497.6,0c-261.7,228.1-338.9,942.1-60.3,1110.7s400.5-89,998.6,151.2c478.5,192.1,484.1-185.4,484.1-185.4v3.5c0,0-1910.5,5.1-1920,0V345.3V0' },
-            ],
-            easing: 'easeInOutQuint',
-            duration: 1300,
-            loop: false,
-            autoplay: false
-
-        });
-        morphing.play();
-    }
-
 
     render() {
-        this.playAnimation();
+        this.props.playAnimation(window.location.pathname);
         return (
             <div className="container text-center">
 
