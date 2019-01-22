902f3ae976bbc48f3ef12169b324f16155731277
fajfara <a.fajfar95@gmail.com>
2019-01-23 00:02:24 +0100

    Before merge

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,18 +1,48 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import React, { Component } from 'react';/* 
+import { Link } from 'react-router-dom'; */
 
 export class Contact extends Component {
+    state = {
+        name: 'name',
+        email: 'email',
+        message: 'message'
+    }
     componentWillMount() {
         this.props.playAnimation(window.location.pathname);
 
     }
 
+    handleChange = (e) => {
+        this.setState({
+            [e.target.id]: e.target.value
+        });
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(this.state);
+    }
+
     render() {
         return (
             <div className="contact-us container text-center">
-                <h1>This is the contact page</h1>
+                <form id="contact" onSubmit={this.handleSubmit}>
+                    <div className="form-group">
+                        <label htmlFor="name">Your name: </label>
+                        <input type="text" className="form-control" id="name" placeholder="Enter your name..." onChange={this.handleChange} />
+                    </div>
 
-                <Link className="button submit-form-button" to="/">SEND MESSAGE</Link>
+                    <div className="form-group">
+                        <label htmlFor="email">Your email: </label>
+                        <input type="email" className="form-control" id="email" placeholder="Enter your email..." onChange={this.handleChange} />
+                    </div>
+
+                    <div className="form-group">
+                        <label htmlFor="message">Message</label>
+                        <textarea className="form-control" id="message" rows="5" placeholder="Enter your message..." onChange={this.handleChange}></textarea>
+                    </div>
+                    <button className="submit-form-button button" name="submit" type="submit" id="contact-submit" data-submit="...Sending">SEND YOUR MESSAGE</button>
+                </form>
             </div>
         )
     }
